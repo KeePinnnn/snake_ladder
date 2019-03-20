f1420f11048b63baeaf159b3e1c0a7af375f47a7
KeePinnnn <contact@keepin.info>
2019-03-21 00:05:17 +0800

    added snakes with random values being populated

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # snake_ladder
+
+This is a project purely being done to understand low level programming language such as C++. 
+
+This project will start off by creating a simple snake and ladder game followed by introducing multithreading so that multiplayer can play together. 
---
+++ Snakes.cpp
@@ -0,0 +1,66 @@
+#include <string>
+#include <iostream>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+class Snakes {
+    map<int, int> snake_dict;
+    vector<int> snake_vector;
+    public: 
+        void set_list();
+};
+
+void Snakes::set_list() {
+    vector<int>::iterator pointer;
+    pointer = snake_vector.begin();
+    for (int i = 0; i < 20; i++) {
+        snake_vector.push_back(rand() % 100);
+    }
+
+    while(true) {
+        sort(snake_vector.begin(), snake_vector.end());
+        snake_vector.erase(unique(snake_vector.begin(), snake_vector.end()), snake_vector.end());
+
+        if (snake_vector.size() != 20) {
+            for (int n = snake_vector.size(); n < 20; n++) {
+                snake_vector.push_back(rand() % 100);
+            }
+        }
+        else {
+            break;
+        }
+    }
+
+    for (auto i = snake_vector.begin(); i != snake_vector.end(); i++) 
+        cout << *i << endl;
+
+    cout << "start of sorting" << endl;
+    while (snake_vector.size() > 0) {
+        int head_index = rand() % snake_vector.size();
+        int head_value = snake_vector[head_index];
+        snake_vector.erase(snake_vector.begin() + head_index);
+        
+        int value_index = rand() % snake_vector.size();
+        int value_value = snake_vector[value_index];
+        snake_vector.erase(snake_vector.begin() + value_index);
+
+        snake_dict[head_value] = value_value;
+    }
+
+    // testing to see if the list works
+    // for (auto i = snake_vector.begin(); i != snake_vector.end(); i++) 
+    //     cout << *i << endl;
+
+    // testing to see if dict works 
+    for (auto element : snake_dict) {
+        cout << element.first << " after first, here is second " << element.second << "\n";
+    }
+}
+
+// for testing purpose
+// int main() {
+//     Snakes s = Snakes();
+//     s.set_list();
+// }
